2ad90120d616299b9fbef291f9f09ebf0215f40a
hahn <zbh_yu_jian@163.com>
2024-06-30 20:32:29 +0800

    fix:GrpcCallEvent参数赋值问题

--- src/AbstractClient.php
+++ src/AbstractClient.php
@@ -59,7 +59,7 @@ class AbstractClient
                     $this->logger->debug("[grpc]{$method} [status-code]{$status} [error]code:{$reply->getCode()} message:{$reply->getMessage()} [instance]{$client->getHostName()}");
                 }
                 // Dispatch gRPC Call
-                $this->dispatcher->dispatch(new GrpcCallEvent($status, $method, $status != StatusCode::OK ? $reply->getMessage() : '', (float)microtime(true) - $startAt), $client->getHostName());
+                $this->dispatcher->dispatch(new GrpcCallEvent($status, $method, $status != StatusCode::OK ? $reply->getMessage() : '', (float)microtime(true) - $startAt,$client->getHostName()));
                 // return
                 return [$reply, $status, $response];
             } else {
